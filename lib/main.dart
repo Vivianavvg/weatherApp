cdb096b648d6c260a1e395653422f100c2f69da2
Phi Le <phizzle2003@gmail.com>
2025-02-05 19:26:24 -0500

    Final testing and adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,13 @@ class WeatherApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const WeatherHomePage(title: 'Weather App'),
+      home: const WeatherHomePage(),
     );
   }
 }
 
 class WeatherHomePage extends StatefulWidget {
-  const WeatherHomePage({super.key, required this.title});
-
-  final String title;
+  const WeatherHomePage({super.key});
 
   @override
   WeatherHomePageState createState() => WeatherHomePageState();
@@ -44,10 +42,12 @@ class WeatherHomePageState extends State<WeatherHomePage> {
 
   void _fetchWeather() {
     setState(() {
-      //Placeholder: replace with actual API call Later
+      if (_cityController.text.isEmpty || _cityController.text == cityName) {
+        return;
+      }
       cityName =
           _cityController.text.isNotEmpty ? _cityController.text : 'Unknown';
-      temperature = '${15 + Random().nextInt(16)}C';
+      temperature = '${15 + Random().nextInt(16)}°C';
       weatherCondition = conditions[Random().nextInt(conditions.length)];
     });
   }
@@ -55,13 +55,13 @@ class WeatherHomePageState extends State<WeatherHomePage> {
   void _getWeekForecast() {
     setState(() {
       _weekForecast = [
-        {'day': 'Sunday', 'temp': '65'},
-        {'day': 'Monday', 'temp': '70'},
-        {'day': 'Tuesday', 'temp': '72'},
-        {'day': 'Wednesday', 'temp': '75'},
-        {'day': 'Thursday', 'temp': '73'},
-        {'day': 'Friday', 'temp': '70'},
-        {'day': 'Saturday', 'temp': '68'},
+        {'day': 'Sunday', 'temp': '18'},
+        {'day': 'Monday', 'temp': '21'},
+        {'day': 'Tuesday', 'temp': '22'},
+        {'day': 'Wednesday', 'temp': '24'},
+        {'day': 'Thursday', 'temp': '23'},
+        {'day': 'Friday', 'temp': '21'},
+        {'day': 'Saturday', 'temp': '20'},
       ];
     });
   }
@@ -71,16 +71,19 @@ class WeatherHomePageState extends State<WeatherHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Weather App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
               controller: _cityController,
               decoration: InputDecoration(
-                  labelText: 'Enter City Name', border: OutlineInputBorder()),
+                labelText: 'Enter City Name', border: OutlineInputBorder()),
+              ),
             ),
             const SizedBox(height: 16),
             ElevatedButton(
@@ -148,6 +151,7 @@ class SevenDayForecast extends StatelessWidget {
         color: const Color.fromARGB(255, 223, 223, 223),
         shape: BoxShape.rectangle,
         borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.blueGrey, width: 2),
       ),
       child: SizedBox(
         width: 300,
@@ -183,7 +187,7 @@ class SevenDayForecast extends StatelessWidget {
                             Padding(
                               padding: const EdgeInsets.all(8.0),
                               child: Text(
-                                'Temp (°F)',
+                                'Temp (°C)',
                                 style: TextStyle(fontWeight: FontWeight.bold),
                               ),
                             ),
